2641a9f9929e7e10089bf445461e3df957d75137
Mohamad-glitch <mohammad.altalib37@gmail.com>
2025-04-22 12:18:47 +0300

    removed

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from routers import farm_routs, user_routs, JWTtoken
 
 from dotenv import load_dotenv
 from fastapi import Depends, FastAPI, HTTPException, Query
-from openai import OpenAI
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from starlette.responses import HTMLResponse
 from starlette.staticfiles import StaticFiles
@@ -18,7 +17,7 @@ from models.user import User, UserPublic, UserCreate
 
 sql_file_name = "farm_database.db"
 sql_url = f"sqlite:///./{sql_file_name}"
-connect_args = {"check_same_thread": False} # recommended by FastAPI docs
+connect_args = {"check_same_thread": False}  # recommended by FastAPI docs
 
 #connecting database
 engine = create_engine(sql_url, connect_args=connect_args)
@@ -31,7 +30,7 @@ def create_db_and_tables():
 # This is what ensures that we use a single session per request.
 def get_session():
     with Session(engine) as session:
-        yield session #yield that will provide a new Session for each request.
+        yield session  #yield that will provide a new Session for each request.
 
 
 # we create an Annotated dependency SessionDep to simplify the rest of the code that will use this dependency.
@@ -45,7 +44,3 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(farm_routs.router)
 app.include_router(user_routs.router)
 app.include_router(JWTtoken.router)
-
-
-
-
