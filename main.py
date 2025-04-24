21f77b8b2e956a0541907bf7fa5efdbd13013257
Mohamad-glitch <mohammad.altalib37@gmail.com>
2025-04-24 20:41:40 +0300

    re

--- main.py
+++ main.py
@@ -55,4 +55,10 @@ async def get_index():
     """Show Home Page."""
     return HTMLResponse(content=open("static/index.html").read())
 
+@app.get("/register", response_class=HTMLResponse)
+async def register():
+    """Register User."""
+    return HTMLResponse(content=open("static/register.html").read())
+
+
 
