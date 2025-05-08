d70ed8c9d3e958a988b3587eaf31cdea149bca1e
Mohamad-glitch <mohammad.altalib37@gmail.com>
2025-05-09 02:20:22 +0300

    chat bot is working

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from starlette.staticfiles import StaticFiles
 from routers import JWTtoken
 from routers import farm_routs, user_routs
 
-#whatever
+#whatever new 
 
 #let the server gets the api key from the server(render)
 if os.getenv("RENDER") != "true":
